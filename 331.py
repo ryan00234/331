2cbc751bbf9715c3bed9cf69790df065ec0d606d
ryan00234 <si09022@yahoo.co.jp>
2017-04-06 19:21:29 +0800

    no message

--- 331.py
+++ 331.py
@@ -1,47 +1,46 @@
-from flask import Flask, request, redirect, make_response, render_template
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
 from datetime import datetime
-from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
-moment = Moment(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
-@app.route('/')
-def index():
-    user_agent = request.headers.get('User-Agent')
-    return render_template('index.html', user_agent=user_agent, current_time=datetime.utcnow())
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-@app.route('/baidu')
-def baidu():
-    return redirect('http://www.baidu.com')
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('404.html'), 500
+    return render_template('500.html'), 500
 
-@app.route('/response')
-def responser():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
+@app.route('/debug')
+def user():
+    return render_template('debug_ui_172.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    user_agent = request.headers.get('User-Agent')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), user_agent=user_agent, current_time=datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=9527, debug=True)
