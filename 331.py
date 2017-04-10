7fc08fb8fe5ef7880554d71bf8e15ae31569ce97
ryan00234 <si09022@yahoo.co.jp>
2017-04-10 13:37:48 +0800

    no message

--- 331.py
+++ 331.py
@@ -36,7 +36,7 @@ def SQL():
 
 @app.route('/test')
 def test():
-    return render_template('temp.html')
+    return render_template('static/docs/examples/navbars.html')
 
 @app.route('/Android')
 def Android():
