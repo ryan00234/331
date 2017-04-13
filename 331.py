347c8d45d04141e73b6979a3b4c2f85fae8ad517
ryan00234 <si09022@outlook.com>
2017-04-14 00:02:06 +0800

    Update 331.py

--- 331.py
+++ 331.py
@@ -1,53 +1,113 @@
-from flask import Flask, render_template, request, jsonify
-import random
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
+{% extends "base.html" %}
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+{% block page_content %}
 
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Highstock Example</title>
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+		<script src="https://cdn.hcharts.cn/jquery/jquery-1.8.3.min.js"></script>
+		<style>
+${demo.css}
+		</style>
+	</head>
+	<body>
+<script src="https://cdn.hcharts.cn/highstock/5.0.10/highstock.js"></script>
+<script src="https://cdn.hcharts.cn/highstock/5.0.10/modules/exporting.js"></script>
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
+<div id="container" style="height: 400px; width: 900px"></div>
 
-@app.route('/debug')
-def debug():
-    return render_template('debug_ui_172.html')
+<script>
+Highcharts.setOptions({
+    global: {
+        useUTC: false
+    }
+});
 
-@app.route('/SQL')
-def SQL():
-    return render_template('SQL.html')
+// Create the chart
+Highcharts.stockChart('container', {
+    chart: {
+        type: 'areaspline',
+            animation: Highcharts.svg, // don't animate in old IE
+            marginRight: 10,
+        events: {
+            load: function () {
 
-@app.route('/test')
-def test():
-    return render_template('temp.html')
+                // set up the updating of the chart each second
+                var series = this.series[0];
+                setInterval(function () {
+                    $.getJSON("http://127.0.0.1:9527/dynamic", { }, function(json){
+                        var x = (new Date()).getTime(), // current time
+                        y = json.cpu/1000
+                    series.addPoint([x, y], true, true);
+                    });
+                }, 600);
+            }
+        }
+    },
 
-@app.route('/dynamic')
-def add_numbers():
-    label = ["January", "February", "March", "April", "May", "June", "July"]
-    data1 = random.sample(range(100), 7)
-    data2 = random.sample(range(100), 7)
-    return jsonify(label, data1, data2)
+    rangeSelector: {
+        buttons: [{
+            count: 1,
+            type: 'minute',
+            text: '1M'
+        }, {
+            count: 5,
+            type: 'minute',
+            text: '5M'
+        }, {
+            type: 'all',
+            text: 'All'
+        }],
+        inputEnabled: false,
+        selected: 0
+    },
 
-@app.route('/Android')
-def Android():
-    return render_template('Android.html',)
+    title: {
+        text: 'CPU Data'
+    },
+    exporting: {
+        enabled: false
+    },
+       credits:{
+        enabled:false
+    },
+    plotOptions: {
+            area: {
+                fillOpacity: 0.2, // 指定所有面积图的透明度
+                pointStart: 1940,
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
+                    }
+                }
+            }
+        },
+    series: [{
+        name: 'CPU Data',
+        data: (function () {
+            // generate an array of random data
+            var data = [],
+                time = (new Date()).getTime(),
+                i;
 
-@app.route('/debug2')
-def debug2():
-    return render_template('debug_connection_172.html')
+            for (i = -999; i <= 0; i += 1) {
+                data.push([
+                    time + i * 600,
+                    0
+                ]);
+            }
+            return data;
+        }())
+    }]
+});
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=9527, debug=True)
+		</script>
+	</body>
+{% endblock %}
