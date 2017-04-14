91fb86ca512904b4aa309c25c5d70f4c05aa31b2
ryan00234 <si09022@outlook.com>
2017-04-14 18:09:24 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
-from flask_sockets import Sockets
+import os
+pkg_name = 'bf.cloud.bfclouddemowithui'
 
+def cpu():
+    os.popen('adb wait-for-device')
+    cmd_cpu = os.popen('adb shell dumpsys cpuinfo |grep bf.cloud.bfclouddemowithui')
+    for i in cmd_cpu.readlines():
+        cpuinfo = i.split(' ')
+        user = float(cpuinfo[4][:-1])
+        kernel = float(cpuinfo[7][:-1])
+    return (user + kernel)
 
-app = Flask(__name__)
-sockets = Sockets(app)
-app.config.from_object('config')
-from app import views
